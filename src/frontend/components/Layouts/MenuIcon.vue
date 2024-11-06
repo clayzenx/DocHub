50375acb89ee277d4a499cf2e539c9709f22002b
clayzenx <clay.zenx@gmail.com>
2024-11-06 15:25:43 +0300

    Лого и иконка меню

---
+++ src/frontend/components/Layouts/MenuIcon.vue
@@ -0,0 +1,87 @@
+<template>
+  <div v-bind:class="$style.menu">
+    <svg
+      viewBox="0 0 252 252"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink">
+      <defs />
+      <circle
+        id="Oval 30"
+        cx="126.000000"
+        cy="126.000000"
+        r="126.000000"
+        fill="#FFFFFF"
+        fill-opacity="1.000000" />
+      <circle
+        id="Oval 30"
+        cx="126.000000"
+        cy="126.000000"
+        r="120.000000"
+        stroke="#2C8353"
+        stroke-opacity="1.000000"
+        stroke-width="12.000000" />
+      <circle
+        id="Oval 31"
+        cx="57.000000"
+        cy="87.000000"
+        r="9.000000"
+        fill="#2C8353"
+        fill-opacity="1.000000" />
+      <rect
+        id="Rectangle 327051"
+        x="74.000000"
+        y="78.000000"
+        rx="9.000000"
+        width="130.000000"
+        height="18.000000"
+        fill="#2C8353"
+        fill-opacity="1.000000" />
+      <circle
+        id="Oval 31"
+        cx="57.000000"
+        cy="126.000000"
+        r="9.000000"
+        fill="#2C8353"
+        fill-opacity="1.000000" />
+      <rect
+        id="Rectangle 327051"
+        x="74.000000"
+        y="117.000000"
+        rx="9.000000"
+        width="130.000000"
+        height="18.000000"
+        fill="#2C8353"
+        fill-opacity="1.000000" />
+      <circle
+        id="Oval 31"
+        cx="57.000000"
+        cy="166.000000"
+        r="9.000000"
+        fill="#2C8353"
+        fill-opacity="1.000000" />
+      <rect
+        id="Rectangle 327051"
+        x="74.000000"
+        y="157.000000"
+        rx="9.000000"
+        width="130.000000"
+        height="18.000000"
+        fill="#2C8353"
+        fill-opacity="1.000000" />
+    </svg>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style module>
+.menu {
+  padding: 4px;
+  border-radius: 17px;
+  width: 36px;
+  height: 36px;
+}
+</style>
