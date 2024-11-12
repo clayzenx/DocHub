d71f932a25963bc5303c925c7f3da637abd50df2
Vladislav Markin <Markin.V.Ya@sberbank.ru>
2024-11-13 01:22:19 +0300

    Прогрев датасетов для кластерного режима

---
+++ src/backend/cluster/validators_warmup.mjs
@@ -0,0 +1,15 @@
+import datasets from '../helpers/datasets.mjs';
+import { logger } from '../utils/logger/index.mjs';
+
+const LOG_TAG = 'validators_warmup';
+
+export default async function(app) {
+
+    const datasetsForApp = datasets(app);
+
+    for (const key in app.storage.manifest.datasets) {
+        await datasetsForApp.getData(app.storage.manifest, { origin: key, source: '({})' });
+    }
+
+    logger.log('Validators cache warm up finished', LOG_TAG, 'info');
+}
--- src/backend/storage/manager.mjs
+++ src/backend/storage/manager.mjs
@@ -11,6 +11,7 @@ import jsonataDriver from '../helpers/jsonata.mjs';
 import jsonataFunctions from '../../global/jsonata/functions.mjs';
 import {newManifest, loader, isRolesMode, DEFAULT_ROLE} from '../utils/rules.mjs';
 import uriTool from '../helpers/uri.mjs';
+import validatorsWarmup from '../cluster/validators_warmup.mjs';
 
 const LOG_TAG = 'storage-manager';
 
@@ -157,6 +158,9 @@ export default {
 		app.storage = storage;  // Инициализируем данные хранилища
 		this.resetCustomFunctions(storage.manifest);
 		app.storage.roles = [];
+		if (isCluster && isPrimary) {
+			validatorsWarmup(app);
+		}
 		if (!isCluster || isPrimary) {
 			await validators(app);        // Выполняет валидаторы
 		}
